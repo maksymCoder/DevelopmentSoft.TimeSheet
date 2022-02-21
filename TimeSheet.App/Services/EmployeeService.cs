ac63f47ad8dd4aa0af393ad41da458ba18209b80
Admin <Admin@DESKTOP-VTO995M>
2022-02-21 13:12:32 +0200

    #21.02

---
+++ TimeSheet.App/Services/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeSheet.Domain.Models;
+
+namespace TimeSheet.App.Services
+{
+    public class EmployeeService
+    {
+        private readonly IEmployeeRepository _employeeREpository;
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeREpository = employeeRepository;
+        }
+    }
+}
---
+++ TimeSheet.DataAccess.csv/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using TimeSheet.Domain.Models;
+
+namespace TimeSheet.DataAccess.csv
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private const string PATH = "..\\..\\DevelopmentSoft.TimeSheet\\TimeSheet.DataAccess.csv\\Data\\Dataemployees.csv";
+        private const char DELIMETER = ';';
+
+        public void Add(StaffEmployee employee)
+        {
+            var dataRow = $"{employee.LastName} " +
+                          $"{DELIMETER}{employee.Salary}\n";
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            File.AppendAllText(PATH, dataRow, Encoding.GetEncoding(1251));
+        }
+        public StaffEmployee GetEmployee(string lastName)
+        {
+            return new StaffEmployee();
+        }
+    }
+}
